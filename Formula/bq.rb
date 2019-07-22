95618f25d7e956a5ef937f3e3fe4284feaa1c80b
Travis CI <travis@Traviss-Mac-80.local>
2019-07-22 10:12:11 +0000

    Update formula version

--- Formula/bq.rb
+++ Formula/bq.rb
@@ -1,7 +1,7 @@
 class Bq < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
-  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.8-osx.zip"
+  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.10-osx.zip"
   sha256 "147ac0a350608d6cf548c70b5ec05c9df8b432cedd23b341ff088e86168c3119"
 
   bottle :unneeded
