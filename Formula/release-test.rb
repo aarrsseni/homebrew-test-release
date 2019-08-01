7a603a30e2b9b9162f86513685aa27a0de183a98
Travis CI <travis@Traviss-Mac-80.local>
2019-08-01 14:10:17 +0000

    Update formula version

--- Formula/release-test.rb
+++ Formula/release-test.rb
@@ -1,8 +1,8 @@
 class ReleaseTest < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
-  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.23.zip"
-  sha256 "145ac51286628524108d62f26301fc8f3d0ef969002274f4e85af7d649a8ff757f"
+  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.24.zip"
+  sha256 "14b3dc3ae02cb01cb70620050ffc12dd316cf8853d830fd52dd5d5c83deb1233e9"
 
   bottle :unneeded
 
