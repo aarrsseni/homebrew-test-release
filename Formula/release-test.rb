9dac2c7ddf4493acfada8dcc33f8cdad54015d42
Arseni Bulatski <ancarseni@gmail.com>
2019-08-01 18:02:31 +0300

    Update

--- Formula/release-test.rb
+++ Formula/release-test.rb
@@ -1,8 +1,8 @@
 class ReleaseTest < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
-  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.26.zip"
-  sha256 "14643d4d3375cb440e256b5bf9e82393aa90d2e26206426505158ce630b6d0b30f"
+  url "https://bintray.com/aarrsseni/release-test/download_file?file_path=release-test-1.22-SNAPSHOT.zip"
+  sha256 "143423"
 
   bottle :unneeded
 
